946b3a4f5ec3757d4780a7c444f84ba3d2cc7ef1
Akmal Muhamad Firdaus <coder.newbie04@gmail.com>
2022-10-17 11:39:34 +0700

    Appstarter commit

---
+++ android/app/src/main/java/com/kelompok1/mydoc/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kelompok1.mydoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
